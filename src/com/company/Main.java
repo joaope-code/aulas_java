091b1eb1cecec50e73308ae75d7d7d155fc0b137
joaope-code <jaumundergroud@gmail.com>
2021-07-12 16:25:44 -0300

    teste

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//       6 Scanner scan = new Scanner(System.in);
+//        System.out.println("Qual a area do circulo?");
+//        double raio = scan.nextDouble();
+//
+//        double area = Math.PI * Math.pow(raio, 2);
+//        System.out.println(area);
+
+
+
+
+
+
+
+
+
+
+             }
+}
