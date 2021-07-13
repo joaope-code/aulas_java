8a6a51911bbcc7dc0981ee77a6743dcfa86ada1b
joaope-code <86989613+joaope-code@users.noreply.github.com>
2021-07-13 08:29:30 -0300

    parte 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,25 +5,42 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//       6-Scanner scan = new Scanner(System.in);
-//        System.out.println("Qual a area do circulo?");
-//        double raio = scan.nextDouble();
+//           1-System.out.println("alo mundo de merda, kkk q odio");
 //
-//        double area = Math.PI * Math.pow(raio, 2);
-//        System.out.println(area);
-
-
-//        7-Scanner Scan = new Scanner(System.in);
-//         System.out.println("Informe um lado do quadrado ");
-//         double raio = Scan.nextDouble();
+//           2-int numero = 69;
+//             System.out.println("O numero informado foi : " + numero);
 //
-//         double result = Math.pow(raio, 2);
-//         System.out.println("A área é "+ result);
-//         double res = result*2;
-//         System.out.println(res);
+//           3-int num1 = 25;
+//             int num2 = 50;
+//             System.out.println(num1 + num2);
+
+//           4-int nota1 = 10;
+//             int nota2 = 8;
+//             int nota3 = 9;
+//             int nota4 = 5;
+//             System.out.println((nota1 + nota2 + nota3 + nota4) /4);
+//
+//            5-float metros = 2.67f;
+//              System.out.println("Essa medida em centimetros é : " + metros*100 );
 
 
-       // Scanner scan = new Scanner(System.in);
+//            6-Scanner scan = new Scanner(System.in);
+//              System.out.println("Qual a area do circulo?");
+//              double raio = scan.nextDouble();
+//              double area = Math.PI * Math.pow(raio, 2);
+//              System.out.println(area);
+
+
+//            7-Scanner Scan = new Scanner(System.in);
+//              System.out.println("Informe um lado do quadrado ");
+//              double raio = Scan.nextDouble();
+//              double result = Math.pow(raio, 2);
+//              System.out.println("A área é "+ result);
+//              double res = result*2;
+//              System.out.println(res);
+
+
+       // 8-Scanner scan = new Scanner(System.in);
         //System.out.println("Quanto você ganha por hora trabalhada?");
         //double sal = scan.nextDouble();
 
