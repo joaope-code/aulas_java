85f8ab9f7b3544927dc855cb0f21e99001e8e800
joaope-code <86989613+joaope-code@users.noreply.github.com>
2021-07-14 11:35:07 -0300

    exercicio-5

--- src/com/company/Exercicio_5.java
+++ src/com/company/Exercicio_5.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class Exercicio_5 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Info");
+        System.out.println("Informe a primeira nota do aluno");
+        float n1 = scan.nextFloat();
+        System.out.println("Informe a segunda nota do aluno");
+        float n2 = scan.nextFloat();
+        float avg = (n1 + n2) / 2;
+        if (avg >= 7) {
+        }
+        System.out.println("Aprovado");
+            else if (avg == 10) {
+            System.out.println("Aprovado com Distinção");
+        } else (avg < 7) {
+            System.out.println("Reprovado");
+        }
     }
 }
+
+}
+
+
+
