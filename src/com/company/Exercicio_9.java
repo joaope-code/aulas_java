7d1545d5a8c0d4dc8842cae66f330f81422549b7
joaope-code <86989613+joaope-code@users.noreply.github.com>
2021-07-15 11:31:21 -0300

    feito a lista 2.1

--- src/com/company/Exercicio_9.java
+++ src/com/company/Exercicio_9.java
@@ -16,15 +16,15 @@ public class Exercicio_9 {
 
         if (num1>num2 & num2>num3){
             System.out.println(num1 +"-" +num2+"-"+num3);
-        }else if (num1>num2 & num3>num2){
-            System.out.println(num1 +"-" +num3 +"-");
-        }else if (num2>num1 & num1>num3){
+        }else if (num1>num2 & num1>num3 & num2<num3){
+            System.out.println(num1 +"-" +num3 +"-" +num2);
+        }else if (num2>num1 & num2>num3 & num1>num3){
             System.out.println(num2 +"-" +num1 +"-"+num3);
-        }else if (num2>num1 & num3>num1){
+        }else if (num2>num3 & num2>num1 & num3>num1){
             System.out.println(num2 +"-" +num3 +"-" +num1);
-        }else if (num3>num2 & num2>num1){
+        }else if (num3>num2 & num3>num1 & num2>num1){
             System.out.println(num3 +"-" + num2 +"-" +num1);
-        }else if (num3>num1 & num1>num2){
+        }else if (num3>num1 & num3>num2 & num1>num2){
             System.out.println(num3 + "-" +num1 +"-" +num2);
         }else if (num1==num2 & num2==num3){
 
